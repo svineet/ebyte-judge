760d639f505b30218132783cd97f5dea8337a97f
Anurag Sharma <anssharma61@gmail.com>
2015-10-28 16:11:07 +0530

    C++ execution (currently only for linux)

--- main/executer.py
+++ main/executer.py
@@ -61,4 +61,44 @@ def execute_java(submission):
 
 
 def execute_cpp(submission):
-    pass
+    input_ = submission.question_answered.input_data
+    expected_out = submission.question_answered.expected_output
+
+    # weird random filename to prevent race conditions and
+    # such problems where same file is fought for by
+    # two threads
+    tmp_prog = "tmp"+str(random.random())+".cpp"
+    tmp_file = open(tmp_prog, "w")
+    tmp_file.write(submission.program)
+
+    tmp_in = "tmpin"+str(random.random())+".tmpin"
+    tmpin = open(tmp_in, "wr")
+    tmpin.write(input_)
+
+    tmp_file.close()
+    tmpin.close()
+
+    try:
+        # This is very platform/compiler specific stuff.
+        if os.uname()[0] == 'Linux':
+            subprocess.check_call(["c++", tmp_prog])
+            out = subprocess.check_output(["./a.out"],
+                stdin=open(tmp_in), stderr=subprocess.STDOUT)
+        if out==expected_out:
+            submission.status = Submission.ACCEPTED_ANSWER
+            submission.submitter.score += 100
+            submission.submitter.save()
+            submission.save()
+        else:
+            submission.status = Submission.WRONG_ANSWER
+            submission.save()
+
+        print "input"
+        print input_
+        print "got: "
+        print out
+        print "expected: "
+        print expected_out
+    except subprocess.CalledProcessError:
+        submission.status = Submission.RUNTIME_ERROR
+        submission.save()
