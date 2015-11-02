041a14a2eba795d89256674bb9ea111130d98fac
Sai Vineet <saivineet89@gmail.com>
2015-11-02 22:49:46 +0530

    Made semantic local using bower

--- judge/settings.py
+++ judge/settings.py
@@ -102,4 +102,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 LOGIN_URL = '/main/login'
--- judge/urls.py
+++ judge/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,4 @@ urlpatterns = [
     url(r'^main/', include('main.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
